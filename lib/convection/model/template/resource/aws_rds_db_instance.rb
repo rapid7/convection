8c934e5718ea99c8b983b53b56836ea43a9a14bc
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-02-17 16:15:59 -0500

    Fixes typos -- nesting and typo

--- lib/convection/model/template/resource/aws_rds_db_instance.rb
+++ lib/convection/model/template/resource/aws_rds_db_instance.rb
@@ -4,132 +4,129 @@ module Convection
   module Model
     class Template
       class Resource
-      ##
-      # AWS::RDS::DBInstance
-      ##
-      class RDSDBInstance < Resource
-        include Model::Mixin::Taggable
+        ##
+        # AWS::RDS::DBInstance
+        ##
+        class RDSDBInstance < Resource
+          include Model::Mixin::Taggable
 
-        def initialize(*args)
-          super
-          type 'AWS::RDS::DBInstance'
-        end
+          def initialize(*args)
+            super
+            type 'AWS::RDS::DBInstance'
+          end
 
-        def allocated_storage(value)
-          property('AllocatedStorage', value)
-        end
+          def allocated_storage(value)
+            property('AllocatedStorage', value)
+          end
 
-        def allow_major_version_upgrade(value)
-          property('AllowMajorVersionUpgrade', value)
-        end
+          def allow_major_version_upgrade(value)
+            property('AllowMajorVersionUpgrade', value)
+          end
 
-        def auto_minor_version_upgrade(value)
-          property('AutoMinorVersionUpgrade', value)
-        end
+          def auto_minor_version_upgrade(value)
+            property('AutoMinorVersionUpgrade', value)
+          end
 
-        def availability_zone(value)
-          property('AvailabilityZone', value)
-        end
+          def availability_zone(value)
+            property('AvailabilityZone', value)
+          end
 
-        def backup_retention_period(value)
-          property('BackupRetentionPeriod', value)
-        end
+          def backup_retention_period(value)
+            property('BackupRetentionPeriod', value)
+          end
 
-        def db_instance_class(value)
-          property('DBInstanceClass', value)
-        end
+          def db_instance_class(value)
+            property('DBInstanceClass', value)
+          end
 
-        def db_instance_identifier(value)
-          property('DBInstanceIdentifier', value)
-        end
+          def db_instance_identifier(value)
+            property('DBInstanceIdentifier', value)
+          end
 
-        def db_name(value)
-          property('DBName', value)
-        end
+          def db_name(value)
+            property('DBName', value)
+          end
 
-        def db_parameter_group_name(value)
-          property('DBParameterGroupName', value)
-        end
+          def db_parameter_group_name(value)
+            property('DBParameterGroupName', value)
+          end
 
-        def db_security_groups(value)
-          property('DBSecurityGroups', value)
-        end
+          def db_security_groups(value)
+            property('DBSecurityGroups', value)
+          end
 
-        def db_snapshot_identifier(value)
-          property('DBSnapshotIdentifier', value)
-        end
+          def db_snapshot_identifier(value)
+            property('DBSnapshotIdentifier', value)
+          end
 
-        def db_subnet_group_name(value)
-          property('DBSubnetGroupName', value)
-        end
+          def db_subnet_group_name(value)
+            property('DBSubnetGroupName', value)
+          end
 
-        def engine(value)
-          property('Engine', value)
-        end
+          def engine(value)
+            property('Engine', value)
+          end
 
-        def engine_version(value)
-          property('EngineVersion', value)
-        end
+          def engine_version(value)
+            property('EngineVersion', value)
+          end
 
-        def iops(value)
-          property('Iops', value)
-        end
+          def iops(value)
+            property('Iops', value)
+          end
 
-        def license_model(value)
-          property('LicenseModel', value)
-        end
+          def license_model(value)
+            property('LicenseModel', value)
+          end
 
-        def master_username(value)
-          property('MasterUsername', value)
-        end
+          def master_username(value)
+            property('MasterUsername', value)
+          end
 
-        def master_user_password(value)
-          property('MasterUserPassword', value)
-        end
+          def master_user_password(value)
+            property('MasterUserPassword', value)
+          end
 
-        def multi_az(value)
-          property('MultiAZ', value)
-        end
+          def multi_az(value)
+            property('MultiAZ', value)
+          end
 
-        def option_group_name(value)
-          property('OptionGroupName', value)
-        end
+          def option_group_name(value)
+            property('OptionGroupName', value)
+          end
 
-        def port(value)
-          property('Port', value)
-        end
+          def port(value)
+            property('Port', value)
+          end
 
-        def preferred_backup_window(value)
-          property('PreferredBackupWindow', value)
-        end
+          def preferred_backup_window(value)
+            property('PreferredBackupWindow', value)
+          end
 
-        def preferred_maintenance_window(value)
-          property('PreferredMaintenanceWindow', value)
-        end
+          def preferred_maintenance_window(value)
+            property('PreferredMaintenanceWindow', value)
+          end
 
-        def publicly_accessible(value)
-          property('PubliclyAccessible', value)
-        end
+          def publicly_accessible(value)
+            property('PubliclyAccessible', value)
+          end
 
-        def source_db_instance_identifier(value)
-          property('SourceDBInstanceIdentifier', value)
-        end
+          def source_db_instance_identifier(value)
+            property('SourceDBInstanceIdentifier', value)
+          end
 
-        def storage_type(value)
-          property('StorageType', value)
-        end
+          def storage_type(value)
+            property('StorageType', value)
+          end
 
-        def tags(value)
-          property('Tags', value)
-        end
+          def vpc_security_groups(value)
+            property('VPCSecurityGroups', value)
+          end
 
-        def vpc_security_groups(value)
-          property('VPCSecurityGroups', value)
-        end
-
-        def render(*args)
-          super.tap do |resource|
-            render_tags(resource)
+          def render(*args)
+            super.tap do |resource|
+              render_tags(resource)
+            end
           end
         end
       end
