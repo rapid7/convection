d1a1322dccfba1a701bf1fb19ed39f07664cd2fe
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-02-17 14:49:59 -0500

    Adds AWS::RDS::DBSubnetGroup

---
+++ lib/convection/model/template/resource/aws_rds_db_subnet_group.rb
@@ -0,0 +1,47 @@
+require_relative '../resource'
+
+module Convection
+  module Model
+    class Template
+      class Resource
+        ##
+        # AWS::RDS::DBSubnetGroup
+        ##
+        class RDSDBSubnetGroup< Resource
+          include Model::Mixin::Taggable
+
+          def initialize(*args)
+            super
+            type AWS::RDS::DBSubnetGroup
+          end
+
+          def db_subnet_group_description(value)
+              property('DBSubnetGroupDescription', value)
+          end
+
+          def subnet_ids(value)
+              property('SubnetIds', value)
+          end
+
+          def render(*args)
+            super.tap do |resource|
+              render_tags(resource)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  module DSL
+    ## Add DSL method to template namespace
+    module Template
+      def parameter_group(name, &block)
+        r = Model::Template::Resource::RDSDBSubnetGroup.new(name, self)
+
+        r.instance_exec(&block) if block
+        resources[name] = r
+      end
+    end
+  end
+end
