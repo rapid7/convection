d695b25f32b8be8c8866e9d3f28b4704112e530e
Roy Hodgman <roy_hodgman@rapid7.com>
2015-02-24 15:51:19 -0500

    added iam access key
    added template resources for the iam access key object
    added example, which depends on pull request #8 to be merged so that the iam_user method can be used

---
+++ lib/convection/model/template/resource/aws_iam_access_key.rb
@@ -0,0 +1,36 @@
+require_relative '../resource'
+
+module Convection
+  module Model
+    class Template
+      class Resource
+        ##
+        # AWS::IAM::AccessKey
+        ##
+        class IAMAccessKey < Resource
+          property :serial, 'Serial'
+          property :status, 'Status'
+          property :user_name, 'UserName'
+
+          def initialize(*args)
+            super
+            type 'AWS::IAM::AccessKey'
+          end
+        end
+      end
+    end
+  end
+
+  module DSL
+    ## Add DSL method to template namespace
+    module Template
+      def iam_access_key(name, &block)
+        r = Model::Template::Resource::IAMAccessKey.new(name, self)
+        r.instance_exec(&block) if block
+
+        resources[name] = r
+      end
+    end
+  end
+
+end
