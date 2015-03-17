4f8415442b3df14b6b39063b7fa7ca853e2bad32
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-03-16 21:10:31 -0400

    Adds AWS::IAM::InstanceProfile resource

---
+++ lib/convection/model/template/resource/aws_iam_instance_profile.rb
@@ -0,0 +1,36 @@
+require_relative '../resource'
+
+module Convection
+  module DSL
+    ## Add DSL method to template namespace
+    module Template
+      def iam_instance_profile(name, &block)
+        r = Model::Template::Resource::IAMInstanceProfile.new(name, self)
+        r.instance_exec(&block) if block
+
+        resources[name] = r
+      end
+    end
+  end
+
+  module Model
+    class Template
+      class Resource
+        ##
+        # AWS::IAM::IstanceProfile
+        ##
+        class IAMInstanceProfile < Resource
+          property :path, 'Path'
+
+          # List of references to AWS::IAM::Roles. Currently, a maximum of one role can be assigned to an instance profile.
+          property :role, 'Roles', :array
+
+          def initialize(*args)
+            super
+            type 'AWS::IAM::InstanceProfile'
+          end
+        end
+      end
+    end
+  end
+end
