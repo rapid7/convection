3c8e4f01e830bde7c8a6609d14fd3202c8b8f98d
John Manero <jmanero@rapid7.com>
2015-03-30 18:19:33 -0400

    Expand helpers. Moving in som e repeated code

--- lib/convection/dsl/helpers.rb
+++ lib/convection/dsl/helpers.rb
@@ -1,15 +1,53 @@
 module Convection
   module DSL
     ##
-    # Template DSL
+    # Methods for defining DSL/Models
+    ##
+    module ClassHelpers
+      def attribute(attribute_name)
+        attr_writer attribute_name
+        define_method(attribute_name) do |value = nil|
+          instance_variable_set("@#{ attribute_name }", value) unless value.nil?
+          instance_variable_get("@#{ attribute_name }")
+        end
+      end
+
+      def list(attribute_name)
+        define_method(attribute_name) do |value = nil|
+          collection = instance_variable_get("@#{ attribute_name }")
+          collection << value unless value.nil?
+          collection
+        end
+      end
+
+      # def child(child_name, klass, options = {}, &block)
+      #   attr_reader collection
+      #   define_method(child_name) do |instance_name, &instance_block|
+      #     resource = klass.new(instance_name, self)
+      #     resource.instance_exec(&instance_block) if instance_block
+      #
+      #     instance_exec(resource, &block) if block
+      #     instance_variable_get("@#{ options[:collection] }")[instance_name] = resource if options.include?(:collection)
+      #   end
+      # end
+    end
+
+    ##
+    # Helper methods for creating templates
     ##
     module Helpers
-      def attribute(name)
-        attr_writer name
-        define_method(name) do |value = nil|
-          instance_variable_set("@#{ name }", value) unless value.nil?
-          instance_variable_get("@#{ name }")
+      class << self
+        def included(mod)
+          mod.extend(DSL::ClassHelpers)
         end
+      end
+
+      def camelize(str)
+        str.split('-').map!(&:capitalize).join
+      end
+
+      def stack
+        @template.stack
       end
     end
   end
