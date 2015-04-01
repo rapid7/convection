0647b18421d209bf571326571843764f4fde16f7
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-04-01 14:39:59 -0400

    Adds AWS::Route53::HealthCheck

---
+++ lib/convection/model/template/resource/aws_route53_health_check.rb
@@ -0,0 +1,34 @@
+require_relative '../resource'
+
+module Convection
+  module Model
+    class Template
+      class Resource
+        ##
+        # AWS::Route53::HealthCheck
+        ##
+        class Rout53HealthCheck < Resource
+
+          property :health_check_config, 'HealthCheckConfig'
+
+          def initialize(*args)
+            super
+            type 'AWS::Route53::HealthCheck'
+          end
+        end
+      end
+    end
+  end
+
+  module DSL
+    ## Add DSL method to template namespace
+    module Template
+      def route53_health_check(name, &block)
+        r = Model::Template::Resource::Rout53HealthCheck.new(name, self)
+
+        r.instance_exec(&block) if block
+        resources[name] = r
+      end
+    end
+  end
+end
