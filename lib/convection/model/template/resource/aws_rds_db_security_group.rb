02a65e614dc13df76aecda438f1778c150819384
John Manero <jmanero@rapid7.com>
2015-06-05 18:20:41 -0400

    Agresigly factor down boilerplate DSL in resources

--- lib/convection/model/template/resource/aws_rds_db_security_group.rb
+++ lib/convection/model/template/resource/aws_rds_db_security_group.rb
@@ -10,10 +10,14 @@ module Convection
         class RDSDBSecurityGroup < Resource
           include Model::Mixin::Taggable
 
-          type 'AWS::RDS::DBSecurityGroup', :rds_security_group
+          type 'AWS::RDS::DBSecurityGroup'
           property :description, 'GroupDescription'
           property :vpc, 'EC2VpcId'
-          property :ingress, 'DBSecurityGroupIngress', :type => :list
+
+          def initialize(*args)
+            super
+            @properties['DBSecurityGroupIngress'] = []
+          end
 
           def security_group_ingress(&block)
             # A new code block defines a new ingress group
@@ -23,12 +27,15 @@ module Convection
 
           def ec2_security_group(group, owner)
             @properties['DBSecurityGroupIngress'].last.merge!(
-              { 'EC2SecurityGroupName' => group, 'EC2SecurityGroupOwnerId' => owner }
+              :EC2SecurityGroupName => group,
+              :EC2SecurityGroupOwnerId => owner
             )
           end
 
           def cidr_ip(cidr_block)
-            @properties['DBSecurityGroupIngress'].last.merge!({ 'CIDRIP' => cidr_block })
+            @properties['DBSecurityGroupIngress'].last.merge!(
+              :CIDRIP => cidr_block
+            )
           end
 
           def render(*args)
