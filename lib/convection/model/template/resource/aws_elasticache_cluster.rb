503a676f07d2032f1fd6f1eb6ccda9b7973a2fb3
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-06-13 14:05:25 -0400

    Fixes elasticache tests
    This fixes the elasticsearch_cache_cluster property name

--- lib/convection/model/template/resource/aws_elasticache_cluster.rb
+++ lib/convection/model/template/resource/aws_elasticache_cluster.rb
@@ -9,7 +9,7 @@ module Convection
         ##
         class ElastiCacheCluster < Resource
 
-          type 'AWS::ElastiCache::CacheCluster'
+          type 'AWS::ElastiCache::CacheCluster', :elasticache_cache_cluster
           property :auto_minor_version_upgrade, 'AutoMinorVersionUpgrade'
           property :cache_node_type, 'CacheNodeType'
           property :cache_security_group_names, 'CacheSecurityGroupNames'
