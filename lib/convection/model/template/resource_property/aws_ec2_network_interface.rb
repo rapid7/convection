81f26b316c5dafe1b10f1625e95a9a0488ba191f
Brandon Turner <brandon_turner@rapid7.com>
2015-09-16 23:49:05 -0500

    Add support for declaring network interfaces on ec2 instances
    This allows you to do something like:
    
        ec2_instance 'MyInstance' do
          image_id 'ami-12345678'
          instance_type 't2.micro'
          network_interface do
            associate_public_ip_address true
            security_group 'sg-12345678'
            subnet 'subnet-12345678'
          end
          network_interface do
            associate_public_ip_address false
            security_group 'sg-87654321'
            subnet 'subnet-12345678'
          end
        end
    
    to associate one or more network interfaces.  This is the only way to
    set the AssociatePublicIpAddress (as far as I know).

---
+++ lib/convection/model/template/resource_property/aws_ec2_network_interface.rb
@@ -0,0 +1,22 @@
+require_relative '../resource_property'
+
+module Convection
+  module Model
+    class Template
+      class ResourceProperty
+        # Represents an {http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-network-iface-embedded.html EC2 NetworkInterface Embedded Property Type}
+        class EC2NetworkInterface < ResourceProperty
+          property :associate_public_ip_address, 'AssociatePublicIpAddress'
+          property :delete_on_termination, 'DeleteOnTermination'
+          property :description, 'Description'
+          property :device_index, 'DeviceIndex'
+          property :group_set, 'GroupSet', :type => :list
+          alias_method :security_group, :group_set
+          property :private_ip_address, 'PrivateIpAddress'
+          property :secondary_private_ip_address_count, 'SecondaryPrivateIpAddressCount'
+          property :subnet, 'SubnetId'
+        end
+      end
+    end
+  end
+end
