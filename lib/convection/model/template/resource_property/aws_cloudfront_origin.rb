8f4d554ed959b1124a73403f6fc4fbea97c44854
Brandon Turner <brandon_turner@rapid7.com>
2015-10-21 18:48:34 -0500

    Add CloudFront custom origin support

--- lib/convection/model/template/resource_property/aws_cloudfront_origin.rb
+++ lib/convection/model/template/resource_property/aws_cloudfront_origin.rb
@@ -7,10 +7,17 @@ module Convection
         # Represents a {http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-origin.html
         # CloudFront Origin Embedded Property Type}
         class CloudFrontOrigin < ResourceProperty
+          property :custom_origin, 'CustomOriginConfig'
           property :domain_name, 'DomainName'
           property :id, 'Id'
           property :origin_path, 'OriginPath'
           property :s3_origin, 'S3OriginConfig'
+
+          def custom_origin(&block)
+            origin = ResourceProperty::CloudFrontCustomOrigin.new(self)
+            origin.instance_exec(&block) if block
+            properties['CustomOriginConfig'].set(origin)
+          end
 
           def s3_origin(&block)
             origin = ResourceProperty::CloudFrontS3Origin.new(self)
