510401899ef26a97c3f98f79fa5368895689f3fa
Brandon Turner <brandon_turner@rapid7.com>
2015-10-21 20:52:33 -0500

    Add CloudFront example usage

--- lib/convection/model/template/resource/aws_cloudfront_distribution.rb
+++ lib/convection/model/template/resource/aws_cloudfront_distribution.rb
@@ -6,7 +6,38 @@ module Convection
       class Resource
         ##
         # AWS::CloudFront::Distribution
-        ##
+        #
+        # Creates an Amazon CloudFront web distribution.   See
+        # {http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution.html AWS::CloudFront::Distribution}.
+        #
+        # == Example
+        #
+        # cloudfront_distribution 'MySiteWebsite' do
+        #   config do
+        #   cname 'mysite.example.com'
+        #   default_root_object 'index.html'
+        #   price_class 'PriceClass_100'
+        #   default_cache_behavior do
+        #     forwarded_values do
+        #       query_string false
+        #     end
+        #     target_origin 's3-mysite-bucket'
+        #     viewer_protocol_policy 'redirect-to-https'
+        #   end
+        #   origin do
+        #     id 's3-mysite-bucket'
+        #     domain_name "mysite.example.com.s3-website-#{stack.region}.amazonaws.com"
+        #     custom_origin do
+        #       protocol_policy 'http-only'
+        #     end
+        #   end
+        #   viewer_certificate do
+        #     iam_certificate 'EXAMPLECERTID'
+        #     minimum_protocol_version 'TLSv1'
+        #     ssl_support_method 'sni-only'
+        #   end
+        # end
+        #
         class CloudFrontDistribution < Resource
           include Model::Mixin::Taggable
 
