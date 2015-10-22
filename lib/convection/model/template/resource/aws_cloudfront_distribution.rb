aa05dbce283b2dffca5ccb52aafb098487284e04
Brandon Turner <brandon_turner@rapid7.com>
2015-10-21 20:56:09 -0500

    Fix CloudFront documentation indentation

--- lib/convection/model/template/resource/aws_cloudfront_distribution.rb
+++ lib/convection/model/template/resource/aws_cloudfront_distribution.rb
@@ -14,27 +14,28 @@ module Convection
         #
         # cloudfront_distribution 'MySiteWebsite' do
         #   config do
-        #   cname 'mysite.example.com'
-        #   default_root_object 'index.html'
-        #   price_class 'PriceClass_100'
-        #   default_cache_behavior do
-        #     forwarded_values do
-        #       query_string false
+        #     cname 'mysite.example.com'
+        #     default_root_object 'index.html'
+        #     price_class 'PriceClass_100'
+        #     default_cache_behavior do
+        #       forwarded_values do
+        #         query_string false
+        #       end
+        #       target_origin 's3-mysite-bucket'
+        #       viewer_protocol_policy 'redirect-to-https'
         #     end
-        #     target_origin 's3-mysite-bucket'
-        #     viewer_protocol_policy 'redirect-to-https'
-        #   end
-        #   origin do
-        #     id 's3-mysite-bucket'
-        #     domain_name "mysite.example.com.s3-website-#{stack.region}.amazonaws.com"
-        #     custom_origin do
-        #       protocol_policy 'http-only'
+        #     origin do
+        #       id 's3-mysite-bucket'
+        #       domain_name "mysite.example.com.s3-website-#{stack.region}.amazonaws.com"
+        #       custom_origin do
+        #         protocol_policy 'http-only'
+        #       end
         #     end
-        #   end
-        #   viewer_certificate do
-        #     iam_certificate 'EXAMPLECERTID'
-        #     minimum_protocol_version 'TLSv1'
-        #     ssl_support_method 'sni-only'
+        #     viewer_certificate do
+        #       iam_certificate 'EXAMPLECERTID'
+        #       minimum_protocol_version 'TLSv1'
+        #       ssl_support_method 'sni-only'
+        #     end
         #   end
         # end
         #
