678703ac385d9548e7800af0b99ce72d7dddefca
Nicholas Davis <nicholas_davis@rapid7.com>
2015-10-27 11:18:46 -0400

    added lambda template

---
+++ lib/convection/model/template/resource/aws_lambda.rb
@@ -0,0 +1,22 @@
+require_relative '../resource'
+
+module Convection
+  module Model
+    class Template
+      class Resource
+        ##
+        # AWS::Lambda::Function
+        ##
+        class Lambda < Resource
+          type 'AWS::Lambda::Function'
+          property :code, 'Code'
+          property :description, 'Description'
+          property :handler, 'Handler',
+          property :memory_size, 'MemorySize'
+          property :runtime, 'Runtime'
+          property :timeout, 'Timeout'
+        end
+      end
+    end
+  end
+end
