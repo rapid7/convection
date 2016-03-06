598ac3eee94b6fdd149cfdac4f259830cd97cf9c
Brandon Turner <brandon_turner@rapid7.com>
2016-03-06 01:01:29 -0600

    Add support for EC2::Instance block device mappings

--- lib/convection/model/template/resource/aws_ec2_instance.rb
+++ lib/convection/model/template/resource/aws_ec2_instance.rb
@@ -22,6 +22,7 @@ module Convection
           property :src_dst_checks, 'SourceDestCheck'
           property :disable_api_termination, 'DisableApiTermination'
           property :network_interfaces, 'NetworkInterfaces', :type => :list
+          property :block_devices, 'BlockDeviceMappings', :type => :list
           property :volumes, 'Volumes', :type => :list
 
           # Append a network interface to network_interfaces
@@ -30,6 +31,13 @@ module Convection
             interface.instance_exec(&block) if block
             interface.device_index = network_interfaces.count.to_s
             network_interfaces << interface
+          end
+
+          # Append a block device mapping
+          def block_device(&block)
+            block_device = ResourceProperty::EC2BlockDeviceMapping.new(self)
+            block_device.instance_exec(&block) if block
+            block_devices << block_device
           end
 
           # Append a volume to volumes
