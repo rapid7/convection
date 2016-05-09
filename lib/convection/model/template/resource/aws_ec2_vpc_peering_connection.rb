586ad2dceae0c29bd6ceb34f78bf9685c52be26c
Simon Irwin <simon_irwin@rapid7.com>
2016-05-09 14:38:10 +0100

    #121 - support tags on VPCPeeringConnection

--- lib/convection/model/template/resource/aws_ec2_vpc_peering_connection.rb
+++ lib/convection/model/template/resource/aws_ec2_vpc_peering_connection.rb
@@ -8,9 +8,17 @@ module Convection
         # AWS::EC2::VPCPeeringConnection
         ##
         class EC2VPCPeeringConnection < Resource
+          include Model::Mixin::Taggable
+
           type 'AWS::EC2::VPCPeeringConnection'
           property :vpc, 'VpcId'
           property :peer_vpc, 'PeerVpcId'
+
+          def render(*args)
+            super.tap do |resource|
+              render_tags(resource)
+            end
+          end
         end
       end
     end
