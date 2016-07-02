5456dabe484ecb4453fe4a19951efbf51fa20d4a
Erran Carey <me@errancarey.com>
2016-07-02 19:03:27 +0100

    Use a more realistic event pattern
    This is an anonymised version (don't want anyone having out instance ID
    right?) of one of our patterns.

--- spec/convection/model/template/resource/events_rule_spec.rb
+++ spec/convection/model/template/resource/events_rule_spec.rb
@@ -9,7 +9,9 @@ class Convection::Model::Template::Resource
         # TODO: Test simple properties of EventsRule as well?
         events_rule 'MyEventsRule' do
           event_pattern 'source', %w(aws.ec2)
-          event_pattern 'detail', 'state' => %w(running)
+          event_pattern 'detail',
+                        'state' => %w(shutting-down stopped stopping terminated),
+                        'instance-id' => 'i-deadb33f'
           event_pattern 'detail-type', ['EC2 Instance State-change Notification']
         end
       end
@@ -25,7 +27,10 @@ class Convection::Model::Template::Resource
     it 'combines all event pattern calls into a single event pattern JSON object' do
       expect(subject['EventPattern']).to include(
         'source' => %w(aws.ec2),
-        'detail' => { 'state' => %w(running) },
+        'detail' => {
+          'state' => %w(shutting-down stopped stopping terminated),
+          'instance-id' => 'i-deadb33f'
+        },
         'detail-type' => ['EC2 Instance State-change Notification']
       )
     end
