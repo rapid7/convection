0850145090e7eefa0b23328a9117c19db349d31d
Bryan Call <bryan_call@rapid7.com>
2016-07-07 16:28:25 -0700

    port loggroups to rspec

---
+++ spec/convection/model/template/resource/loggroups_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'pp'
+class Convection::Model::Template::Resource
+  describe Lambda do
+    let(:template) do
+      Convection.template do
+        description 'Logroups Test Template'
+
+        resource 'testgroup' do
+          type 'AWS::Logs::LogGroup'
+          property 'RetentionInDays', 365
+        end
+      end
+    end
+
+    subject do
+      template_json
+    end
+
+    it 'template is defined' do
+      expect(subject).to_not eq(nil)
+    end
+
+    it 'resource values are properly set' do
+      expect(subject['Resources'].include?('testgroup'))
+    end
+
+    private
+
+    def template_json
+      JSON.parse(template.to_json)
+    end
+  end
+end
