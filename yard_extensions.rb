431388c6c5da0d5caeeeb3cb3dadad8ce8eca848
Erran Carey <me@errancarey.com>
2016-07-21 13:38:26 +0100

    Split yard handlers into separate extension files

--- yard_extensions.rb
+++ yard_extensions.rb
@@ -1,30 +1,4 @@
-# A YARD handler to deal with the "property" DSL method.
-class PropertyDslHandler < YARD::Handlers::Ruby::Base
-  handles method_call(:property)
-  namespace_only # Do not process nested method calls.
-
-  def process
-    name = statement.parameters.first.jump(:tstring_content, :ident).source
-    object = YARD::CodeObjects::MethodObject.new(namespace, name)
-    object.parameters = [['value', nil]]
-    register(object)
-
-    cf_property = statement.parameters[1].source
-    docstring = <<-DOC
-@overload #{name}
-
-  Returns the value of the #{cf_property} CloudFormation property.
-@overload #{name}(value)
-
-  Sets the #{cf_property} CloudFormation property.
-
-  @param value the value to set the #{cf_property} CloudFormation property to.
-DOC
-    object.docstring = docstring if object.docstring.blank?(false)
-
-    # modify the object
-    object.dynamic = true
-    object.scope = :class
-    object.name
-  end
+# Load all Ruby files inside of the YARD extensions directory.
+Dir.glob('yard_extensions/**.rb') do |file|
+  require_relative file
 end
---
+++ yard_extensions/type_handler.rb
@@ -0,0 +1,24 @@
+# A YARD handler to deal with the "type" DSL method.
+class TypeDslHandler < YARD::Handlers::Ruby::Base
+  IRREGULAR_SLUGS ||= {
+    'AWS::ElasticLoadBalancing::LoadBalancer' => 'aws-properties-ec2-elb.html'
+  }
+
+  handles method_call(:type)
+  namespace_only # Do not process nested method calls.
+
+  def process
+    name = call_params.first.dup
+    slug = slug_for_name(name)
+    url = "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/#{slug}"
+    namespace.docstring << "\n\nSee also the CloudFormation documentation for {#{url} #{name}}."
+  end
+
+  private
+
+  def slug_for_name(name)
+    return IRREGULAR_SLUGS[name] if IRREGULAR_SLUGS.key?(name)
+
+    name.downcase.gsub('::', '-').sub('aws-', 'aws-resource-').concat('.html')
+  end
+end
