4d354708a89698837adf35256113f0eb92f4ceaf
Erran Carey <me@errancarey.com>
2016-07-25 19:03:55 +0100

    Add PoC CustomResource (group) class

---
+++ lib/convection/model/template/custom_resource.rb
@@ -0,0 +1,45 @@
+require_relative './resource'
+
+module Convection
+  module Model
+    class Template
+      # TODO: We've been back on forth on the name for this concept.
+      # Is CustomResource *really* better than ResourceGroup?
+      class CustomResource
+        include DSL::Helpers
+        include DSL::IntrinsicFunctions
+        include DSL::Template::Resource
+
+        attribute :type
+        attr_reader :name
+        attr_reader :parent
+        attr_reader :template
+
+        def initialize(name, parent, &block)
+          @name = name
+          @parent = parent
+          @template = parent.template
+
+          template.attach_resource(@name, self.class)
+          attach_resource(@name, self.class)
+          instance_exec(&block) if block
+        end
+
+        def render(stack)
+          resources.map do |resource|
+            if resource.is_a?(Convection::Model::Template::CustomResource)
+              resource.render(stack)
+            else
+              # TODO: Just make Resource#render accept stack as a [today] unused argument?
+              resource.render
+            end
+          end
+        end
+
+        def resources
+          @resources ||= template.resources.dup
+        end
+      end
+    end
+  end
+end
