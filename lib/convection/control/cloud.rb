25a66e39d3d592a19b5884a4c7dd7702162e8223
Erran Carey <me@errancarey.com>
2016-07-26 18:59:14 +0100

    Simplify Cloud#converge/#diff yet again

--- lib/convection/control/cloud.rb
+++ lib/convection/control/cloud.rb
@@ -16,13 +16,15 @@ module Convection
         @cloudfile.stacks
       end
 
-      def deck(included_stacks = [])
-        return @cloudfile.deck unless included_stacks.any?
-        @cloudfile.deck.select { |k, _v| included_stacks.include?(stack_name) }.to_h
-      end
-
       def stack_groups
         @cloudfile.stack_groups
+      end
+
+      def deck(options = {})
+        included_stacks = stack_groups[options[:stack_groups]]
+        included_stacks ||= options.fetch(:stack_list, [])
+        return @cloudfile.deck unless included_stacks.any?
+        @cloudfile.deck.select { |k, _v| included_stacks.include?(k) }.to_h
       end
 
       def converge(to_stack, options = {}, &block)
@@ -31,42 +33,35 @@ module Convection
           return
         end
 
-        deck.each do |stack|
-          break unless apply_converge(stack, &block)
+        deck(options).each do |stack|
+          block.call(Model::Event.new(:converge, "Stack #{ stack.name }", :info)) if block
+          stack.apply(&block)
+          if stack.error?
+            block.call(Model::Event.new(:error, "Error converging stack #{ stack.name }", :error), stack.errors) if block
+            return false
+          end
+
           ## Stop on converge error
           break unless stack.success?
+
           ## Stop here
           break if !to_stack.nil? && stack.name == to_stack
           sleep rand @cloudfile.splay || 2
         end
-
       end
 
       def diff(to_stack, options = {}, &block)
-        deck.each do |stack|
-          break unless generate_diff(stack, &block)
+        deck(options).each do |stack|
+          block.call(Model::Event.new(:compare, "Compare local state of stack #{ stack.name } (#{ stack.cloud_name }) with remote template", :info))
+          difference = stack.diff
+          if difference.empty?
+            difference << Model::Event.new(:unchanged, "Stack #{ stack.cloud_name } Has no changes", :info)
+          end
+          difference.each { |diff| block.call(diff) }
+
           break if !to_stack.nil? && stack.name == to_stack
           sleep rand @cloudfile.splay || 2
         end
-      end
-
-      def apply_converge(stack, &block)
-        block.call(Model::Event.new(:converge, "Stack #{ stack.name }", :info)) if block
-        stack.apply(&block)
-        if stack.error?
-          block.call(Model::Event.new(:error, "Error converging stack #{ stack.name }", :error), stack.errors) if block
-          return false
-        end
-        stack.success?
-      end
-
-      def generate_diff(stack, &block)
-        block.call(Model::Event.new(:compare, "Compare local state of stack #{ stack.name } (#{ stack.cloud_name }) with remote template", :info))
-        difference = stack.diff
-        if difference.empty?
-          difference << Model::Event.new(:unchanged, "Stack #{ stack.cloud_name } Has no changes", :info)
-        end
-        difference.each { |diff| block.call(diff) }
       end
     end
   end
