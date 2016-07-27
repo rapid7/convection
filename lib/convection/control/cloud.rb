172f1da738dd92614c31040f1095a52e47002fb0
Erran Carey <me@errancarey.com>
2016-07-27 11:56:37 +0100

    Add private helper methods for #included_stacks/#nonexistent_stacks

--- lib/convection/control/cloud.rb
+++ lib/convection/control/cloud.rb
@@ -21,14 +21,13 @@ module Convection
       end
 
       def filter_deck(options = {}, &block)
-        included_stacks = stack_groups[options[:stack_group]]
-        included_stacks ||= options[:stack_list]
-        return @cloudfile.deck if included_stacks.nil?
+        included_stacks = included_stacks(options)
+        return @cloudfile.deck if included_stacks(options).empty?
 
+        nonexistent_stacks = nonexistent_stacks(options)
         stack_list = @cloudfile.stacks.map { |name, stack| stack if included_stacks.include?(name) }.compact
-        nonexistent_stacks = included_stacks.reject { |name| @cloudfile.stacks.key?(name) }
-
         block.call(Model::Event.new(:error, "Stack(s) #{nonexistent_stacks.join(',')} did not exist", :error)) if nonexistent_stacks.any?
+
         stack_list
       end
 
@@ -65,6 +64,19 @@ module Convection
           break if !to_stack.nil? && stack.name == to_stack
           sleep rand @cloudfile.splay || 2
         end
+      end
+
+      private
+
+      def included_stacks(options = {})
+        included_stacks = stack_groups[options[:stack_group]]
+        included_stacks ||= options[:stack_list]
+
+        Array(included_stacks)
+      end
+
+      def nonexistent_stacks(options = {})
+        included_stacks(options).reject { |name| @cloudfile.stacks.key?(name) }
       end
     end
   end
