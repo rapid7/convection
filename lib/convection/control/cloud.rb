c220eb1a8e52b23da416b93de2f18232c613c89b
Bryan Call <bryan_call@rapid7.com>
2016-09-08 15:43:57 -0700

    initial attempt to add some rate limititng controls

--- lib/convection/control/cloud.rb
+++ lib/convection/control/cloud.rb
@@ -117,7 +117,7 @@ module Convection
         filter_deck(options, &block).each_value do |stack|
           block.call(Model::Event.new(:compare, "Compare local state of stack #{ stack.name } (#{ stack.cloud_name }) with remote template", :info))
 
-          difference = stack.diff
+          difference = stack.diff_caller
           if difference.empty?
             difference << Model::Event.new(:unchanged, "Stack #{ stack.cloud_name } has no changes", :info)
           end
