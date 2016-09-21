92884d304d9fc231e29b124a409d40a12debc79a
abegley-r7 <Alan_Begley@rapid7.com>
2016-09-21 14:41:37 +0100

    PD-8267 fix linting errors.

--- lib/convection/model/template/resource_property/aws_s3_replication_configuration.rb
+++ lib/convection/model/template/resource_property/aws_s3_replication_configuration.rb
@@ -4,8 +4,8 @@ module Convection
   module Model
     class Template
       class ResourceProperty
-        #Represents an {https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-replicationconfiguration.html}
-        #Amazon S3 Replication Configuration
+        # Represents an {https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-replicationconfiguration.html}
+        # Amazon S3 Replication Configuration
         class S3ReplicationConfiguration < ResourceProperty
           property :role, 'Role'
           property :rules, 'Rules', :type => :list
