4e2fc36534d3f849b95ff52e9288b3a602169d51
Andrew Thompson <Andrew_Thompson@rapid7.com>
2017-01-20 14:21:17 -0500

    Failing test for #234
    We were not properly testing the interaction between `Cloudfile` and
    `Stack`.  So this slipped though...

--- spec/convection/model/cloudfile_spec.rb
+++ spec/convection/model/cloudfile_spec.rb
@@ -3,11 +3,32 @@ require 'stringio'
 
 module Convection::Model
   describe Cloudfile do
+    let(:template) do
+      Convection.template do
+        description 'EC2 VPC Test Template'
+
+        ec2_vpc 'TargetVPC' do
+          network '10.0.0.0'
+          subnet_length 24
+          enable_dns
+        end
+      end
+    end
+
     describe 'stack' do
       subject do
-        klass = Class.new
-        klass.extend(Convection::DSL::Cloudfile)
-        klass
+        # initialize does too much to call that directly
+        # setup only what we need for testing... :/
+        class TestCloudfile < Convection::Model::Cloudfile
+          def initialize(_)
+            @attributes = Convection::Model::Attributes.new
+            @stacks = {}
+            @deck = []
+            @stack_groups = {}
+            @thread_count ||= 2
+          end
+        end
+        TestCloudfile.new(nil)
       end
 
       it 'can get cloud name' do
@@ -23,6 +44,12 @@ module Convection::Model
       it 'can exclude_availability_zones an availability_zone' do
         subject.exclude_availability_zones %w(eu-central-1a)
         expect(subject.exclude_availability_zones).to contain_exactly('eu-central-1a')
+      end
+
+      it 'can get default exclude_availability_zones' do
+        subject.stack('test-stack', template, :region => 'us-east-1')
+        stack = subject.stacks['test-stack']
+        expect(stack.exclude_availability_zones).to match_array([])
       end
     end
   end
