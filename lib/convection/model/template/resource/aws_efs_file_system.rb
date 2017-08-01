93063cfb84285c81b5b666c1a05cf16c389a5eb9
Frank Mitchell <me@frankmitchell.org>
2017-08-01 16:48:19 -0400

    Use Taggable mixin instead of rolling my own code.

--- lib/convection/model/template/resource/aws_efs_file_system.rb
+++ lib/convection/model/template/resource/aws_efs_file_system.rb
@@ -7,24 +7,14 @@ module Convection
         # Represents an {https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html
         # Amazon EFS File System}
         class EFSFileSystem < Resource
+          include Model::Mixin::Taggable
+
           type 'AWS::EFS::FileSystem'
           property :performance_mode, 'PerformanceMode'
 
-          def file_system_tags(value = nil)
-            return @file_system_tags if value.nil?
-            @file_system_tags = Convection::Model::Tags[value]
-          end
-
-          def file_system_tag(key, value)
-            @file_system_tags ||= Convection::Model::Tags.new
-            @file_system_tags[key] = value
-          end
-
           def render(*args)
             super.tap do |resource|
-              unless file_system_tags.nil? || file_system_tags.empty?
-                resource['Properties']['FileSystemTags'] = file_system_tags.render
-              end
+              resource['Properties']['FileSystemTags'] = tags.render unless tags.empty?
             end
           end
         end
--- spec/convection/model/template/resource/aws_efs_file_system_spec.rb
+++ spec/convection/model/template/resource/aws_efs_file_system_spec.rb
@@ -11,8 +11,8 @@ class Convection::Model::Template::Resource
 
     it 'allows FileSystemTags to be set' do
       expect(subject.render['Properties']['FileSystemTags']).to be_nil
-      subject.file_system_tags([['key-0', 'value-0']])
-      subject.file_system_tags({'key-1'=>'value-1', 'key-2'=>'value-2'})
+      subject.tag 'key-1', 'value-1'
+      subject.tag 'key-2', 'value-2'
       expect(subject.render['Properties']['FileSystemTags']).to eq([{
         'Key'=>'key-1', 'Value'=>'value-1'
       },{
@@ -24,17 +24,6 @@ class Convection::Model::Template::Resource
       expect(subject.render['Properties']['PerformanceMode']).to be_nil
       subject.performance_mode 'maxIO'
       expect(subject.render['Properties']['PerformanceMode']).to eq('maxIO')
-    end
-
-    it 'allows individual FileSystemTags to be set' do
-      expect(subject.render['Properties']['FileSystemTags']).to be_nil
-      subject.file_system_tag 'key-1', 'value-1'
-      subject.file_system_tag 'key-2', 'value-2'
-      expect(subject.render['Properties']['FileSystemTags']).to eq([{
-        'Key'=>'key-1', 'Value'=>'value-1'
-      },{
-        'Key'=>'key-2', 'Value'=>'value-2'
-      }])
     end
   end
 end
