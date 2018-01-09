36502dd348506ce310e6d7d4ca5c3bc137d22fee
Frank Mitchell <me@frankmitchell.org>
2018-01-09 09:32:36 -0500

    Export AWS::EC2::Route objects as Terraform Route resources.

--- lib/convection/model/template/resource/aws_ec2_route.rb
+++ lib/convection/model/template/resource/aws_ec2_route.rb
@@ -16,6 +16,32 @@ module Convection
           property :instance, 'InstanceId'
           property :interface, 'NetworkInterfaceId'
           property :peer, 'VpcPeeringConnectionId'
+
+          def to_hcl_json(*)
+            tf_record_attrs = {
+              route_table_id: route_table_id,
+              destination_cidr_block: destination,
+              vpc_peering_connection_id: peer,
+              gateway_id: gateway,
+              nat_gateway_id: nat_gateway,
+              instance_id: instance,
+              network_interface_id: interface
+            }
+
+            tf_record_attrs.reject! { |_, v| v.nil? }
+
+            tf_record = {
+              aws_route: {
+                name.underscore => tf_record_attrs
+              }
+            }
+
+            { resource: tf_record }.to_json
+          end
+
+          def terraform_import_commands(module_path: 'root')
+            ['# Route import is not supported by Terraform.']
+          end
         end
       end
     end
