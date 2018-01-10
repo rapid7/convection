66c2a86723f8e822a68495d03b424a7a6c822890
Erran Carey <me@errancarey.com>
2018-01-10 16:10:21 +0000

    Add terraform import support for aws_iam_user
    Signed-off-by: mburns-r7 <Michael_Burns@rapid7.com>

--- lib/convection/model/template/resource/aws_iam_user.rb
+++ lib/convection/model/template/resource/aws_iam_user.rb
@@ -63,6 +63,54 @@ module Convection
           property :path, 'Path'
           property :policies, 'Policies', :type => :list
           property :user_name, 'UserName'
+
+          def terraform_import_commands(module_path: 'root')
+            prefix = "#{module_path}." unless module_path == 'root'
+            resource_id = stack.resources[name] && stack.resources[name].physical_resource_id
+            commands = ['# Import the aws_iam_user:']
+            commands << "terraform import #{prefix}aws_iam_user.#{name.underscore} #{resource_id}"
+
+            if Array(policies).any?
+              commands << ''
+              commands << '# Unable to import aws_iam_user_policy resources (inline policies).'
+              commands << "# Future versions of terraform may support import using:"
+            end
+            Array(policies).each do |policy|
+              commands << "# terraform import #{prefix}aws_iam_user_policy.#{policy.name.underscore} #{resource_id}:#{policy.name}"
+            end
+
+            commands << '# Unable import any iam user login profiles' if Array(login_profile).any?
+            Array(login_profile).each do |profile|
+              commands << "# Please ensure that the '#{profile}' login profile has an associated aws_iam_user_login_profile with the #{resource_id} user in terraform configuration."
+            end
+
+            commands << '# Import the policy attachments (managed policies):' if Array(managed_policy_arn).any?
+            Array(managed_policy_arn).each do |policy|
+              commands << "# Please ensure that the '#{policy}' managed policy has an associated aws_iam_user_policy_attachment with the #{resource_id} user in terraform configuration."
+            end
+
+            commands << '# Associate user with groups:' if Array(group).any?
+            Array(group).each do |group_name|
+              commands << "# Please ensure that the #{resource_id} user is associated with the '#{group_name}' group in terraform configuration."
+            end
+
+            commands
+          end
+
+          def to_hcl_json(*)
+            resources = []
+            actual_user_name = user_name || (stack.resources[name] && stack.resources[name].physical_resource_id)
+            resources << {
+              aws_iam_user: {
+                name.underscore => {
+                  name: actual_user_name,
+                  path: path,
+                }
+              }
+            }
+
+            { resource: resources }.to_json
+          end
         end
       end
     end
