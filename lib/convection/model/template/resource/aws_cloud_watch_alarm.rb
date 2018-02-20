63e0b2b83b0f313688555c238ffa7b33a82826b1
Mike Burns <michael_burns@rapid7.com>
2018-02-20 15:44:01 +0000

    added to_hcl support for aws_cloud_watch_alarm

--- lib/convection/model/template/resource/aws_cloud_watch_alarm.rb
+++ lib/convection/model/template/resource/aws_cloud_watch_alarm.rb
@@ -24,6 +24,41 @@ module Convection
           property :statistic, 'Statistic'
           property :threshold, 'Threshold'
           property :unit, 'Unit'
+
+          def terrafom_import_commands(module_path: 'root')
+            commands = ['Run the following commands to import your infrastructure into terraform management.', '# ensure :module_path is set correctly', '']
+            commands << "terraform import #{module_prefix}aws_cloudwatch_metric_alarm.#{name.underscore} #{stack.resources[name].physical_resource_id}"
+            commands << ''
+            commands
+          end
+
+          def to_hcl_json(*)
+            tf_alarm_attrs = {
+              alarm_name: alarm_name,
+              comparison_operator: comparison_operator,
+              evaluation_periods: evaluation_periods,
+              metric_name: metric_name,
+              namespace: namespace,
+              period: period,
+              statistic: statistic,
+              threshold: threshold,
+              actions_enabled: actions_enabled,
+              alarm_actions: alarm_action,
+              alarm_description: alarm_description,
+              dimensions: dimension,
+              insufficient_data_actions: insufficient_data_action,
+              ok_actions: ok_action,
+              unit: unit
+            }
+
+            tf_alarm = {
+              aws_cloudwatch_metric_alarm: {
+                name.underscore => tf_alarm_attrs
+              }
+            }
+
+            { resource: [tf_alarm] }.to_json
+          end
         end
       end
     end
