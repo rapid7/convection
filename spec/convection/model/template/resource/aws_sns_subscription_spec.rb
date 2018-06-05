72f4b85c68a0591d43e14a9bdb8e905c390db597
Eric Romani <eric_romani@rapid7.com>
2018-06-05 15:29:26 -0700

    Fixed spacing and whitespace

--- spec/convection/model/template/resource/aws_sns_subscription_spec.rb
+++ spec/convection/model/template/resource/aws_sns_subscription_spec.rb
@@ -1,41 +1,40 @@
 require 'spec_helper'
 
 class Convection::Model::Template::Resource
-    describe SNSSubscription do
-        let(:template) do
-            Convection.template do
-              sns_subscription 'MySubscription' do
-                endpoint 'failures@example.com'
-                protocol 'email'
-                topic_arn 'arn:aws:sns:us-west-2:123456789012:example-topic'
-              end
-            end
+  describe SNSSubscription do
+    let(:template) do
+      Convection.template do
+        sns_subscription 'MySubscription' do
+          endpoint 'failures@example.com'
+          protocol 'email'
+          topic_arn 'arn:aws:sns:us-west-2:123456789012:example-topic'
         end
-
-        subject do
-            template_json
-              .fetch('Resources')
-              .fetch('MySubscription')
-              .fetch('Properties')
-        end
-
-        it 'sets the Endpoint' do
-          expect(subject['Endpoint']).to eq('failures@example.com')
-        end
-
-        it 'sets the Protocol' do
-          expect(subject['Protocol']).to eq('email')
-        end
-
-        it 'sets the TopicArn' do
-          expect(subject['TopicArn']).to eq('arn:aws:sns:us-west-2:123456789012:example-topic')
-        end
-
-
-      private
-
-      def template_json
-         JSON.parse(template.to_json)
-       end
+      end
     end
+
+    subject do
+      template_json
+        .fetch('Resources')
+        .fetch('MySubscription')
+        .fetch('Properties')
+    end
+
+    it 'sets the Endpoint' do
+      expect(subject['Endpoint']).to eq('failures@example.com')
+    end
+
+    it 'sets the Protocol' do
+      expect(subject['Protocol']).to eq('email')
+    end
+
+    it 'sets the TopicArn' do
+      expect(subject['TopicArn']).to eq('arn:aws:sns:us-west-2:123456789012:example-topic')
+    end
+
+    private
+
+    def template_json
+      JSON.parse(template.to_json)
+    end
+  end
 end
