073d733176dd479f97040c752c1c9a6279607854
Eric Romani <eric_romani@rapid7.com>
2018-08-27 13:49:48 -0700

    Add filter policy property to sns subscriptions resource

--- lib/convection/model/template/resource/aws_sns_subscription.rb
+++ lib/convection/model/template/resource/aws_sns_subscription.rb
@@ -9,12 +9,14 @@ module Convection
         #     endpoint 'failures@example.com'
         #     protocol 'email'
         #     topic_arn 'arn:aws:sns:us-west-2:123456789012:example-topic'
+        #     filter_policy 'Type' => 'Notification', 'MessageId' => 'e3c4e17a-819b-5d95-a0e8-b306c25afda0', 'MessageAttributes' => [{' AttributeName' => 'Name', 'KeyType' => 'HASH' }],
         #   end
         class SNSSubscription < Resource
           type 'AWS::SNS::Subscription'
           property :endpoint, 'Endpoint'
           property :protocol, 'Protocol'
           property :topic_arn, 'TopicArn'
+          property :filter_policy, 'FilterPolicy'
         end
       end
     end
